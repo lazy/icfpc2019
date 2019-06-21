d24827ec588c5585213b82e8f76175bf782f956a
Ivan Korostelev <ivkorost@microsoft.com>
2019-06-21 13:12:38 +0100

    FromAscii implementation. Without tests so far

--- Solution/Map.cs
+++ Solution/Map.cs
@@ -1,5 +1,7 @@
 ﻿namespace Icfpc2019.Solution
 {
+    using System;
+
     public struct Map
     {
         private readonly Cell[,] cells;
@@ -30,5 +32,40 @@
         public int StartY { get; }
 
         public Cell this[int i, int j] => this.cells[i, j];
+
+        public static Map FromAscii(int startX, int startY, params string[] lines)
+        {
+            var cells = new Cell[lines.Length, lines[0].Length];
+            for (var i = 0; i < lines.Length; ++i)
+            {
+                if (lines[i].Length != lines[0].Length)
+                {
+                    throw new ArgumentException("All lines must be of equal length");
+                }
+
+                for (var j = 0; j < lines[i].Length; ++j)
+                {
+                    cells[i, j] = AsciiToCell(lines[i][j]);
+                }
+            }
+
+            return new Map(startX, startY, cells);
+        }
+
+        private static Cell AsciiToCell(char c)
+        {
+            switch (c)
+            {
+                case ' ': return Cell.Empty;
+                case '#': return Cell.Obstacle;
+                case '!': return Cell.Edge;
+                case 'B': return Cell.BoosterB;
+                case 'F': return Cell.BoosterF;
+                case 'D': return Cell.BoosterD;
+                case 'X': return Cell.BoosterX;
+                default:
+                    throw new ArgumentOutOfRangeException($"Invalid cell ascii representation: {c}");
+            }
+        }
     }
 }
