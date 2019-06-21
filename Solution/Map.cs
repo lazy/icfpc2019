32ccea1078dfc7a345fafe3d1888e9bc11c4ec4f
Ivan Korostelev <ivkorost@microsoft.com>
2019-06-21 12:30:19 +0100

    Map

---
+++ Solution/Map.cs
@@ -0,0 +1,28 @@
+﻿namespace Solution
+{
+    public struct Map
+    {
+        private readonly Cell[,] cells;
+
+        public Map(Cell[,] cells)
+        {
+            this.cells = cells;
+        }
+
+        public enum Cell
+        {
+            Empty,
+            Obstacle,
+            Edge,
+            BoosterB,
+            BoosterF,
+            BoosterD,
+            BoosterX,
+        }
+
+        public int Width => this.cells.GetLength(0);
+        public int Height => this.cells.GetLength(1);
+
+        public Cell this[int i, int j] => this.cells[i, j];
+    }
+}
