f41b93382e6b7e87ea13701a14cb03a947b8a57b
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-06-21 15:45:05 +0100

    Move the Point struct to a separate file

--- Solution/MapParser.cs
+++ Solution/MapParser.cs
@@ -116,29 +116,6 @@ namespace Icfpc2019.Solution
             };
         }
 
-        private struct Point : IEquatable<Point>
-        {
-            public int X { get; set; }
-
-            public int Y { get; set; }
-
-            public static bool operator !=(Point left, Point right) => !left.Equals(right);
-
-            public static bool operator ==(Point left, Point right) => left.Equals(right);
-
-            public bool Equals(Point other) => this.X == other.X && this.Y == other.Y;
-
-            public override bool Equals(object obj) => obj is Point other && this.Equals(other);
-
-            public override int GetHashCode()
-            {
-                unchecked
-                {
-                    return (this.X * 397) ^ this.Y;
-                }
-            }
-        }
-
         private class AllPoints
         {
             public AllPoints()
---
+++ Solution/Point.cs
@@ -0,0 +1,27 @@
+namespace Icfpc2019.Solution
+{
+    using System;
+
+    public struct Point : IEquatable<Point>
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public static bool operator !=(Point left, Point right) => !left.Equals(right);
+
+        public static bool operator ==(Point left, Point right) => left.Equals(right);
+
+        public bool Equals(Point other) => this.X == other.X && this.Y == other.Y;
+
+        public override bool Equals(object obj) => obj is Point other && this.Equals(other);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.X * 397) ^ this.Y;
+            }
+        }
+    }
+}
