7678e2b7e779ee229583328a9de38112bb2464fa
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-06-22 05:26:50 +0100

    Unskip the test

--- Tests/EmulatorTests.cs
+++ Tests/EmulatorTests.cs
@@ -20,7 +20,7 @@
             @"xv.........x",
             @"xxxxxxxxxxxx");
 
-        [Fact(Skip="Map.AreVisible() is limited for now")]
+        [Fact]
         public void SampleSolution1IsValid() =>
             this.TestOnSampleMap(
                 "WDWB(1,2)DSQDB(-3,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW",
