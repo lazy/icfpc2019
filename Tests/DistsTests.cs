c1382a4a3630fcb5043534c41ca1910577c62c29
Ivan Korostelev <ivkorost@microsoft.com>
2019-06-23 20:09:43 +0100

    Useful visuals

---
+++ Tests/DistsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Icfpc2019.Tests
+{
+    using System.IO;
+
+    using Icfpc2019.Solution;
+
+    using Xunit;
+
+    public class DistsTests
+    {
+        [Fact]
+        public void CanCalcDists()
+        {
+            var map = MapParser.Parse(File.ReadAllText("../../../../Data/maps/prob-077.desc"));
+            var dists = new DistsFromCenter(new State(map));
+            var bmp = dists.SaveToBitmap();
+            bmp.Save("../../../../prob.png");
+        }
+    }
+}
