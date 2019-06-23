4c20055fe28abd1403d9935de8af0b7b67ac3152
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-06-23 13:19:50 +0100

    a pretty test

--- Scripts/plot_score_dynamics.py
+++ Scripts/plot_score_dynamics.py
@@ -12,9 +12,13 @@ if __name__ == '__main__':
 
 	xs = range(3, current_block + 1)
 	ys = []
+	prev_balance = None
 	for bn in xs:
 		block_balance = int(requests.get(f'{BLOCKCHAIN_URL}/getblockinfo/{bn}').json()['balances'].get(public_id, 0))
+		if prev_balance == block_balance:
+			print(f'Block {bn - 1} was invalid')
 		ys.append(block_balance)
+		prev_balance = block_balance
 
 	plt.plot(xs, ys)
 	plt.savefig('balance.png')
--- Tests/PuzzleTests.cs
+++ Tests/PuzzleTests.cs
@@ -6,9 +6,17 @@ namespace Icfpc2019.Tests
     using Icfpc2019.Solution;
 
     using Xunit;
+    using Xunit.Abstractions;
 
     public class PuzzleTests
     {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public PuzzleTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void TestValidation()
         {
@@ -19,6 +27,31 @@ namespace Icfpc2019.Tests
             var puzzle15 = new Puzzle(File.ReadAllText(@"..\..\..\..\Data\blocks\15\puzzle.cond"));
             var map15 = @"..\..\..\..\Data\blocks\15\puzzle.cond.desc";
             puzzle15.EnsureMapIsValid(map15);
+        }
+
+        [Fact]
+        public void ValidateAllBlocks()
+        {
+            for (var bn = 3; bn <= 45; ++bn)
+            {
+                if (bn == 39)
+                {
+                    // The puzzle solver hangs on this one :(
+                    continue;
+                }
+
+                var puzzle = new Puzzle(File.ReadAllText($@"..\..\..\..\Data\blocks\{bn}\puzzle.cond"));
+                var map = $@"..\..\..\..\Data\blocks\{bn}\puzzle.cond.desc";
+                try
+                {
+                    puzzle.EnsureMapIsValid(map);
+                    this.testOutputHelper.WriteLine($"VALID puzzle solution for block #{bn}");
+                }
+                catch (Exception ex)
+                {
+                    this.testOutputHelper.WriteLine($"INVALID puzzle solution for block #{bn}: {ex.Message}");
+                }
+            }
         }
     }
 }
